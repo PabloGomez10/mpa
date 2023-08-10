6e0c20c5b9eedd91a685eb798c9ec9225035fc9f
PabloGomez10 <102038179+PabloGomez10@users.noreply.github.com>
2023-08-10 19:54:02 -0300

    arregle login y registro, tanto sus mediaquery.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,30 +14,27 @@ function Login() {
   };
   
   return (
-
     <div className="container">
-
       <Row className="justify-content-center">
-        
+  
         <Col md={4}>
-          <img src={ig} alt="Foto de instagram"  className="login-photo" />
+          <img src={ig} alt="Foto de instagram"  className="login-photo w-100" />
         </Col>
 
         <Col md={4}>
-
-           <div className='mt-5'>
-               <Card className='borderL'>
+             <div>
+               <Card className='borderL w-100'>
                    <Card.Body>
                        <form>
-                           <div className='mt-5 p-4'> 
-                               <img src={instagram} className='imgLogin' alt="Instragram"/>
+                           <div className=' p-4'> 
+                               <img src={instagram} className='w-100' alt="Instragram"/>
         
                                <div className="mb-2 form-group mt-1">
                                  <label for="exampleInputEmail1" className="form-label"></label>
                                  <input type="email" className="form-control d-block formularioL" id="exampleInputEmail1"  placeholder="Teléfono, usuario o correo electrónico" aria-describedby="emailHelp"/>
                                </div> 
                                
-                                 <div className="password-toggle-container">
+                                 <div className="password-toggle-container w-100">
                                     <input type={isPasswordVisible ? 'text' : 'password'} id="passwordInput"
                                     className="form-control d-block formularioL" placeholder="Contraseña" />
                                     <button className="toggle-button"type="button" onClick={togglePasswordVisibility}>
@@ -45,26 +42,26 @@ function Login() {
                                     </button>
                                  </div>
                                  
-                                 <div className="form-group text-center mt-3">
+                                 <div className="form-group mt-3">
                                    <button type="submit" className="btn btn-primary loginIniciar">Iniciar Sesión</button>
                                  </div>
 
                                  <div className='loginLC mt-4'>
-                                   <div className='line'></div>
+                                   <div className='line w-100'></div>
                                    <div className='circle'></div>
-                                   <div className='line'></div>
+                                   <div className='line w-100'></div>
                                  </div>
 
                                  <div className='mt-4'>
                                      <div>
-                                          <button className='buttonLoginFacebook'>
+                                          <button className='buttonLoginFacebook w-100'>
                                             <FontAwesomeIcon icon="fa-brands fa-facebook" className='loginFacebook' /> 
-                                            <a   className='letraF' href='https://www.facebook.com/login.php?skip_api_login=1&api_key=124024574287414&kid_directed_site=0&app_id=124024574287414&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Foauth%3Fclient_id%3D124024574287414%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignup%252F%26state%3D%257B%2522fbLoginKey%2522%253A%252216t45wk9ms112mlffgc1jwgolgf11wxb1ks9nbm160a02hpw733c%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%26scope%3Demail%26response_type%3Dcode%252Cgranted_scopes%26locale%3Des_LA%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Dc30b3478-3485-4d68-8514-5c4889d7f272%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fwww.instagram.com%2Faccounts%2Fsignup%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%2522fbLoginKey%2522%253A%252216t45wk9ms112mlffgc1jwgolgf11wxb1ks9nbm160a02hpw733c%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%23_%3D_&display=page&locale=es_LA&pl_dbl=0' target={'_blank'}>
+                                            <a   className='letraF w-100' href='https://www.facebook.com/login.php?skip_api_login=1&api_key=124024574287414&kid_directed_site=0&app_id=124024574287414&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Foauth%3Fclient_id%3D124024574287414%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignup%252F%26state%3D%257B%2522fbLoginKey%2522%253A%252216t45wk9ms112mlffgc1jwgolgf11wxb1ks9nbm160a02hpw733c%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%26scope%3Demail%26response_type%3Dcode%252Cgranted_scopes%26locale%3Des_LA%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Dc30b3478-3485-4d68-8514-5c4889d7f272%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fwww.instagram.com%2Faccounts%2Fsignup%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%2522fbLoginKey%2522%253A%252216t45wk9ms112mlffgc1jwgolgf11wxb1ks9nbm160a02hpw733c%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%23_%3D_&display=page&locale=es_LA&pl_dbl=0' target={'_blank'}>
                                             Iniciar Sesión con Facebook</a>
                                           </button>
                                      </div>
-                                       <button className='buttonLoginFacebook d-flex'>
-                                         <p className='letraO'>¿Olvidate tu contraseña?</p>
+                                       <button className='buttonLoginFacebook d-flex w-100 mb-3'>
+                                         <p className='letraO w-100'>¿Olvidate tu contraseña?</p>
                                        </button>
                                  </div>
                            </div>
@@ -73,7 +70,7 @@ function Login() {
                 </Card>
            </div>
 
-             <Card className='custom-card borderL '>
+             <Card className='mt-1 borderL w-100'>
                  <Card.Body className='text-center'>
                     <p className='loginNC' >¿No tienes una cuenta? <a className='loginRE' href='/registro' target={'_blank'}> Regístrate</a></p>
                  </Card.Body>
@@ -82,16 +79,18 @@ function Login() {
                <div>
                  <h3 className='loginDescargarAPP'> Descarga la app.</h3>
                  <div className='d-flex justify-content-center'> 
-                   <img src={playstore} className='imgLoginDescargas p-1'/>
+                   <img src={playstore} className='imgLoginDescargas'/>
                    <img src={microsoft} className='imgLoginDescargas'/> 
                  </div>
                </div>
 
         </Col>
        </Row>
+       
        <a className='letra' href='/feed'> feed </a>   {/* LUEGO BORRAR ESTA LINEA */}
-               <div className='footer-container'>
-                   <footer className='d-flex justify-content-evenly loginFooter'>
+
+               <div className='mt-5'>
+                   <footer className='d-flex justify-content-evenly loginFooter mt-5'> 
                        <p>Meta</p>
                        <p>Información</p>
                        <p>Blog</p>
@@ -107,8 +106,8 @@ function Login() {
                        <p>Meta verified</p>
                    </footer>
                    
-                   <div className=''>
-                     <footer className='d-flex loginFooter2 justify-content-evenly' >
+                   <div>
+                     <footer className='d-flex loginFooter justify-content-evenly' >
                         <p className='p-3'>Español</p>
                         <p className='p-3'>© 2023 Instagram from Meta</p>
                       </footer>
--- src/pages/registro.js
+++ src/pages/registro.js
@@ -20,36 +20,36 @@ function Registro () {
           <Card  className="mb-5 borderR">
             <Card.Body>
               <form>
-                <div> 
-                  <img src={instagram} className='imgLogin mt-3' alt="Instragram" />
-                  <p className='text-center textR'> Regístrate para ver fotos y videos de tus amigos.</p>
-                  <div className="form-group d-flex justify-content-evenly">
-                    <button type="submit" href='' className="btn btn-primary registroFB ">Iniciar Sesion con Facebook</button>
+                <div className=''> 
+                  <img src={instagram} className='w-100' alt="Instragram" />
+                  <p className='text-center textR w-100'> Regístrate para ver fotos y videos de tus amigos.</p>
+                  <div className="form-group w-100 registroFB">
+                    <button type="submit" href='' className="btn btn-primary ">Iniciar Sesion con Facebook</button>
                   </div>
-                   <div className='loginLC mt-4'>
-                     <div className='line'></div>
-                     <div className='circle'></div>
-                     <div className='line'></div>
+                   <div className='reLC mt-4 w-100'>
+                     <div className='lineR'></div>
+                     <div className='circleR'></div>
+                     <div className='lineR'></div>
                    </div>
 
-                  <div className='mt-4 d-flex justify-content-evenly'>
+                  <div className='mt-3'>
                     <label for="exampleInputEmail1" className='form-label'></label>
-                    <input type="email" className="form-control d-block formularioL" placeholder="Numero de celular o correo electrónico" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input type="email" className="form-control d-block formularioR" placeholder="Numero de celular o correo electrónico" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                   </div> 
 
-                  <div className='mt-2 d-flex justify-content-evenly'>
+                  <div className=''>
                     <label for="exampleInputEmail1" className='form-label'></label>
-                    <input type="email" className="form-control d-block formularioL" placeholder="Nombre Completo" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input type="email" className="form-control d-block formularioR" placeholder="Nombre Completo" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                   </div> 
 
-                  <div className='mt-2 d-flex justify-content-evenly'>
+                  <div className=''>
                     <label for="exampleInputEmail1" className='form-label'></label>
-                    <input type="email" className="form-control d-block formularioL" placeholder="Nombre de usuario"id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input type="email" className="form-control d-block formularioR" placeholder="Nombre de usuario"id="exampleInputEmail1" aria-describedby="emailHelp"/>
                   </div> 
                 
-                   <div className="password-toggle-container mt-2 d-flex justify-content-evenly">
+                   <div className="password-toggle-container mt-4">
                       <input type={isPasswordVisible ? 'text' : 'password'} id="passwordInput"
-                      className="form-control d-block formularioL formularioR" placeholder="Contraseña" />
+                      className="form-control d-block formularioR" placeholder="Contraseña" />
                       <button className="toggle-button"type="button" onClick={togglePasswordVisibility}>
                         {isPasswordVisible ? 'Ocultar' : 'Mostrar'}
                       </button>
@@ -58,9 +58,9 @@ function Registro () {
                     
                     
                      <div>
-                         <div className='registroFont mt-3d-flex justify-content-center'>
+                         <div className='registroFont mt-3 d-flex'>
                            <span>Es posible que las personas que usan nuestro servicio hayan subido tu información de contacto a Instagram. 
-                           <a className='registroLinks'> Más información</a>
+                             <a className='registroLinks'> Más información</a>
                            </span>
                           </div>
                           
@@ -69,10 +69,10 @@ function Registro () {
                                 <a className='registroLinks' >Condiciones </a> 
                                 <span>, la </span>
                                 <a className='registroLinks' > Política de privacidad</a>
-                                <span>y la  </span> 
+                                <span> y la  </span> 
                                 <a className='registroLinks' > Política de cookies.</a>
                              </div>
-                             <div className=' mt-3 mb-4 d-flex justify-content-evenly'> 
+                             <div className=' mt-3 mb-2 w-100'> 
                                <button type="submit" className="btn btn-primary buttonRegistro">Registrarte</button>
                              </div>
                      </div>
@@ -82,42 +82,45 @@ function Registro () {
             </Card.Body>
           </Card>
           
-          <Card  className="borderL registroCard">
+          <Card  className="registroCard">
              <Card.Body className='text-center'>
                  <p>¿Tienes una cuenta? <a className='registroI' href='/'> Inicia sesión</a></p>
              </Card.Body>
           </Card>
 
           
-              <h3 className='loginDescargarAPP mt-3'> Descarga la app.</h3>
-              <div className='d-flex justify-content-evenly asd'>
-              <img src={playstore} className='imgLoginDescargas'/>
-              <img src={microsoft} className='imgLoginDescargas'/> 
+              <h3 className='reDescargarAPP mt-3'> Descarga la app.</h3>
+              <div className='d-flex justify-content-evenly'>
+                <img src={playstore} className='imgReDescargas'/>
+                <img src={microsoft} className='imgReDescargas'/> 
            </div>
 
         </Col>
       </Row>
-             <div className='d-flex justify-content-around loginFooter mt-5'>
-                <p>Meta</p>
-                <p>Información</p>
-                <p>Blog</p>
-                <p>Empleo</p>
-                <p>Ayuda</p>
-                <p>API</p>
-                <p>Privacidad</p>
-                <p>Condiciones</p>
-                <p>Cuentas destacadas</p>
-                <p>Ubicaciones</p>
-                <p>Instagram Lite</p>
-                <p>Subir contactos y no usuarios</p>
-                <p>Meta verified</p>
-             </div>
+      <div className='footer-container'>
+                   <footer className='d-flex justify-content-evenly loginFooter'>
+                       <p>Meta</p>
+                       <p>Información</p>
+                       <p>Blog</p>
+                       <p>Empleo</p>
+                       <p>Ayuda</p>
+                       <p>API</p>
+                       <p>Privacidad</p>
+                       <p>Condiciones</p>
+                       <p>Cuentas destacadas</p>
+                       <p>Ubicaciones</p>
+                       <p>Instagram Lite</p>
+                       <p>Subir contactos y no usuarios</p>
+                       <p>Meta verified</p>
+                   </footer>
                    
-               <div className='d-flex justify-content-evenly loginFooter2'>
-                   <p>Español</p>
-                   <p className='footer3'>© 2023 Instagram from Meta</p>
-               </div>
-               <div className='mt-5'></div>
+                   <div className=''>
+                     <footer className='d-flex loginFooter2 justify-content-evenly' >
+                        <p className='p-3'>Español</p>
+                        <p className='p-3'>© 2023 Instagram from Meta</p>
+                      </footer>
+                   </div>
+                </div>
     </div>
   );
 }
